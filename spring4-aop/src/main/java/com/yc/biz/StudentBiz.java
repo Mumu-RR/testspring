c9a9e1ca33f31b3118a2d60c38bdd0937e5b6229
shier <2766004060@qq.com>
2021-04-11 20:00:08 +0800

    1

--- spring4-aop/src/main/java/com/yc/biz/StudentBiz.java
+++
@@ -1,9 +0,0 @@
-package com.yc.biz;
-
-public interface StudentBiz {
-    int add(String name);
-
-    void update(String name);
-
-    void find(String name);
-}
--- spring5_selfaop_jdkproxy/src/main/java/com/yc/LogAspect.java
+++ spring5_selfaop_jdkproxy/src/main/java/com/yc/LogAspect.java
@@ -2,6 +2,8 @@ package com.yc;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Date;
 
 /**
  * @program: testspring
@@ -16,13 +18,32 @@ public class LogAspect implements InvocationHandler {
         this.target=target;
     }
 
-    @Override
+    @Override //回调方法  当jvm调用代理对象的被代理方法时，会由jvm自动调用这个invoke
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        return null;
+        System.out.println("代理类的对象:"+proxy.getClass());
+        System.out.println("目标类的方法:"+method);
+        System.out.println("方法中的参数:"+args);
+        //转换成切入点表达式的解析_>AspectJ的切入点表达式
+        if(method.getName().startsWith("add")) {
+            //前置增强
+            log();
+        }
+        //相当于直接执行了studentBizImpl中的find();
+        Object returnValue=method.invoke(this.target,args);
+        //后置增强
+
+        return returnValue;
     }
 
-//    public Object createProxy(){
-//        //新建一个代理实例
-//        //return Proxy.newProxyInstance(this.target.getClass().getClassLoader(),this.target)
-//    }
+
+    private void log(){
+        System.out.println("==============before  advice============");
+        System.out.println("hello world is"+new Date());
+        System.out.println("========================================");
+    }
+
+    public Object createProxy(){
+        //新建一个代理实例
+        return Proxy.newProxyInstance(this.target.getClass().getClassLoader(),this.target.getClass().getInterfaces(),this);
+    }
 }
